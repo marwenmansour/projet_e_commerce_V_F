391c264114692a36c0ef127f085dc3386d72cd35
marwen <mansourmarwen@outlook.fr>
2021-12-21 11:24:31 +0100

    connection resolve problem

--- src/Controller/ConnectionController.php
+++ src/Controller/ConnectionController.php
@@ -15,7 +15,7 @@ class ConnectionController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('target_path');
+            return $this->redirectToRoute('acceuil');
         }
 
         // get the login error if there is one
